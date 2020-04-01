d99a920b875c67147e08946bad7507f1a62c4073
Tim Seymore <tim_seymore2012@hotmail.com>
2020-04-01 17:57:20 -0400

    Update nqueens.py

--- Scripts/nqueens.py
+++ Scripts/nqueens.py
@@ -26,3 +26,28 @@ def find_solution(n):
     
 find_solution(8)   
      
+
+# Backtracking method
+
+def can_be_extended_to_solution(perm):
+    i = len(perm) - 1
+    for j in range(i):
+        if i - j == abs(perm[i] - perm[j]):
+            return False
+    return True
+
+def extend(perm, n):
+    if len(perm) == n:
+        print(perm)
+        exit()
+
+    for k in range(n):
+        if k not in perm:
+            perm.append(k)
+
+            if can_be_extended_to_solution(perm):
+                extend(perm, n)
+
+            perm.pop()
+
+# extend(perm = [], n = 13)
