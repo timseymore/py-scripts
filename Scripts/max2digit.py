2439c5a407cc87d5073e731e024de8ed233ee54a
Tim Seymore <tim_seymore2012@hotmail.com>
2020-04-02 13:21:03 -0400

    Update max2digit.py

--- Scripts/max2digit.py
+++ Scripts/max2digit.py
@@ -2,30 +2,35 @@
 """
 Max 2-digit integers
 
+calculate how many 2-digit integers between 1-100 can be added to a list
+so that no two numbers will add to 100
 
 Created on Thu Apr  2 10:25:42 2020
 
 @author: tim_s
 """
 
-def build_num_lst(x, y):
+
+def build_num_lst(x: int, y: int) -> list:
     num_lst = []
     for i in range(x, y+1):
         num_lst.append(i)
     return num_lst
 
-# TODO: use backtracking to find check all permutations
-def sum_under_hundred(lst):
-    ans_lst = []
+
+def check_num(n: int, lst: list) ->  bool:
     for i in lst:
-        if ans_lst == []:
-            ans_lst.append(i)
-        else:
-            for j in ans_lst:
-                if i + j != 100:
-                    print(i)
-                    ans_lst.append(i)
-    return ans_lst
+        if n + i == 100:
+            return False
+    return True
+
+def max2(lst: list, ans_lst: list) -> int:
+    if lst == []:
+        return len(ans_lst)
+    elif ans_lst == [] or check_num(lst[0], ans_lst):
+        ans_lst.append(lst[0])
+    return max2(lst[1::], ans_lst)
+
 
 # print(build_num_lst(10, 99))
-print(sum_under_hundred(build_num_lst(10, 99)))
+print(max2(build_num_lst(10, 99), []))
