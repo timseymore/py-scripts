3227ba0cb39d41d767916f3b81fce180a59c12ad
Tim Seymore <tim_seymore2012@hotmail.com>
2020-04-02 10:48:35 -0400

    Update max2digit.py

--- Scripts/max2digit.py
+++ Scripts/max2digit.py
@@ -10,10 +10,11 @@ Created on Thu Apr  2 10:25:42 2020
 
 def build_num_lst(x, y):
     num_lst = []
-    for i in range(x, y):
+    for i in range(x, y+1):
         num_lst.append(i)
     return num_lst
 
+# TODO: use backtracking to find check all permutations
 def sum_under_hundred(lst):
     ans_lst = []
     for i in lst:
@@ -22,4 +23,5 @@ def sum_under_hundred(lst):
                 ans_lst.append(i)
     return ans_lst
 
-print(sum_under_hundred(build_num_lst(10, 100)))
+# print(build_num_lst(10, 99))
+print(sum_under_hundred(build_num_lst(10, 99)))
