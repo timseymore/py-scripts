6ffac442f2008527f90e811d6efc69017ba47576
Tim Seymore <tim_seymore2012@hotmail.com>
2020-04-06 19:11:35 -0400

    Update factorial.py

--- Scripts/factorial.py
+++ Scripts/factorial.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 """
+Factorial Functions
+
 Created on Mon Apr  6 18:49:51 2020
 
 @author: Tim
@@ -12,3 +14,12 @@ def fact0(n):
     for i in range(1, n + 1):
         result *= i
     return result
+
+
+# Recursive version
+def fact(n):
+    assert(n > 0)
+    if n == 1:
+        return 1
+    else:
+        return n * fact(n - 1)
