45af625e994c5566b56c7d0799973196a4f8799a
Tim Seymore <tim_seymore2012@hotmail.com>
2020-05-10 19:07:47 -0400

    Update curry.py

--- Scripts/curry.py
+++ Scripts/curry.py
@@ -11,12 +11,17 @@ Created on Fri May  8 20:20:39 2020
 """
 
 
-curried_fun = lambda fn, x: fn(x)
+curried_dec = lambda fn, x: fn(x)
 
 curried_add = lambda x: lambda y: x + y
 
-# Test Cases
-# result = curried_fun(len, "hi")
-result = curried_add(2)(5)
 
-print(result) 
+
+
+
+# Test Cases
+
+result1 = curried_dec(len, "hi")
+result2 = curried_add(2)(5)
+
+# print(result1) 
