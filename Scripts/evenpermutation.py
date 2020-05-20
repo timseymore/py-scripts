51515dae1c1a4b5ed75b5f6941df773266da3028
Tim Seymore <tim_seymore2012@hotmail.com>
2020-05-19 20:07:50 -0400

    Update evenpermutation.py

--- Scripts/evenpermutation.py
+++ Scripts/evenpermutation.py
@@ -16,27 +16,20 @@ def is_permutation(p):
 
 
 def is_even(p):
-    # check that given permutation is valid 
-    if is_permutation(p):
+    count = 0    
+    for i in range(len(p)):
+        cursor = p[i]
+        pos = i        
+        while pos > 0 and p[pos - 1] > cursor:
+            # Swap the number down the list
+            p[pos] = p[pos - 1]
+            pos -= 1
+            count += 1
+        # Break and do the final swap
+        p[pos] = cursor
         
-        # recursively sort the given permutation,
-        # keeping count of transpositions
-        def aux(p1, index, acc):
-            if p1 == []:
-                return (acc % 2) == 0                
-            else:    
-                if index != p1[0]:
-                    # it is incorrect
-                    # swap (pi[0] and p1[1])
-                    aux(p1, index, acc + 1)
-                else:
-                    # it is correct
-                    # recursively sort rest of list
-                    aux(p1[1::], index + 1, acc)
-        
-        return aux(p,0,0)
-    else: 
-        return False
+
+    return count % 2 == 0
     
     
 
@@ -50,6 +43,6 @@ print (is_permutation([1,2,3]) == False)
 print (is_permutation([0,1,2,3]) == True)
 
 print(is_even([0]) == True)
-print(is_even([0,2,1]) == True)
-print(is_even([0,1,2,3]) == True)
-print(is_even([0,3,2,1]) == True)
+print(is_even([0,3,2,4,5,6,7,1,9,8]) == True)
+print(is_even( [1, 3, 8, 4, 5, 6, 9, 2, 0, 7]) == False)
+
