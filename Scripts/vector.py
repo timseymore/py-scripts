5ecf3e9249bbf7f3a97c829c28e9d794531b93f0
Tim Seymore <tim_seymore2012@hotmail.com>
2020-05-21 18:52:29 -0400

    Create vector.py

---
+++ Scripts/vector.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+
+Vector
+
+modeling and working with vectors
+
+
+Created on Thu May 21 16:04:45 2020
+
+@author: Tim
+"""
+
+# Vectors are defined as a list of 2 or 3 numbers.
+# The numbers represent the coordinates of the vectors tip .
+# All vectors are assumed to start at origin [0, 0, 0].
+#      X  Y  Z
+V1 = [-1, 2, 0]
+V2 = [ 2, 4, 0]
+
+# Addition of vectors ; using tip to tail method
+V3 = [1, 2, 0]
+W1 = [3, -1, 0]
+V3plW1 = [4, 1, 0] # sum of vectors V3 + W1 ; [x1 + x2, y1 + y2, z1 + z2]
+
+# Multiplication by a number
