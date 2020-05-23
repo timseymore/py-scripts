a7b236a99e743bb1377c75c8365a5a928deb396a
Tim Seymore <tim_seymore2012@hotmail.com>
2020-05-22 21:36:01 -0400

    Update vector.py

--- Scripts/vector.py
+++ Scripts/vector.py
@@ -11,16 +11,29 @@ Created on Thu May 21 16:04:45 2020
 @author: Tim
 """
 
-# Vectors are defined as a list of 2 or 3 numbers.
-# The numbers represent the coordinates of the vectors tip .
-# All vectors are assumed to start at origin [0, 0, 0].
-#      X  Y  Z
-V1 = [-1, 2, 0]
-V2 = [ 2, 4, 0]
+class Vector:
+    def __init__(self, x, y, z=0):
+        self.x = x
+        self.y = y
+        self.z = z
+        
+    def __add__(self, v):
+        return Vector(self.x + v.x, self.y + v.y, self.z + v.z)
+    
+    def __mul__(self, n):
+        return Vector(n*self.x, n*self.y, n*self.z)
+    
+    def __str__(self):
+        return "[" + str(self.x) + ", " + str(self.y) + ", " + str(self.z) + "]"
 
-# Addition of vectors ; using tip to tail method
-V3 = [1, 2, 0]
-W1 = [3, -1, 0]
-V3plW1 = [4, 1, 0] # sum of vectors V3 + W1 ; [x1 + x2, y1 + y2, z1 + z2]
 
-# Multiplication by a number
+
+I_HAT = Vector(1, 0)
+J_HAT = Vector(0, 1)
+
+V1 = Vector(-1, 2)
+V2 = Vector(2, 4)
+V3 = Vector(1, 2, 2)
+V4 = Vector(3, -1, 3)
+
+
