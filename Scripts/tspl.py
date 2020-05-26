1fff390b3bab360bad768a9a2f5ddd3168617832
Tim Seymore <tim_seymore2012@hotmail.com>
2020-05-25 21:38:47 -0400

    Update tspl.py

--- Scripts/tspl.py
+++ Scripts/tspl.py
@@ -14,22 +14,25 @@ Created on Sat May  9 20:24:51 2020
 import sys, os
 
 
+# TODO: change implementation to use pure OOP
+
 class Exp:
-    """ An expression to be evaluated """
+    """ An expression to be evaluated 
     
+    can put default behaviour here    
+    """    
     def __init__(self):
         pass
 
 
-class Const(Exp):
-    """ A constant int """
-    
+class Int(Exp):
+    """ A constant int """    
     def __init__(self, num: int):
         super().__init__()
         self.num = num
         
         
-class Neg(Exp):
+class Negate(Exp):
     """ Negated expression """
     def __init__(self, exp: Exp):
         super().__init__()
@@ -39,15 +42,14 @@ class Neg(Exp):
 class Add(Exp):
     """ Expression representing the result
         of adding two expressions 
-    """
-    
+    """    
     def __init__(self, exp1: Exp, exp2: Exp):
         super().__init__()
         self.exp1 = exp1
         self.exp2 = exp2
       
         
-class Mult(Exp):
+class Multiply(Exp):
     """ Expression representing the result
         of multiplying two expressions 
     """
@@ -57,20 +59,18 @@ class Mult(Exp):
         self.exp2 = exp2
     
     
-class Div(Exp):
+class Divide(Exp):
     """ Expression representing the result
         of dividing two expressions 
-    """
-    
+    """    
     def __init(self, exp1: Exp, exp2: Exp):
         super().__init__()
         self.exp1 = exp1
         self.exp2 = exp2
 
 
-class Var(Exp):
-    """ A variable mapping a string to an expression """
-    
+class Variable(Exp):
+    """ A variable mapping a string to an expression """    
     def __init__(self, name: str, value: Exp):
         super().__init__()
         self.name = name
@@ -82,8 +82,7 @@ class Pair(Exp):
     
        use Nul expression to end list
        ex: Pair(e1, Pair(e2, Nul)) == [e1, e2]
-    """
-    
+    """    
     def __init__(self, exp1: Exp, exp2: Exp):
         super().__init__()
         self.exp1 = exp1
@@ -91,15 +90,13 @@ class Pair(Exp):
         
         
 class Nul(Exp):
-    """ Used to represent a null value """
-    
+    """ Used to represent a null value """    
     def __init__(self):
         super().__init__()
         
 
 class Interpreter:
-    """ TSPL interpreter """
-    
+    """ TSPL interpreter """    
     def __init__(self):
         self.run()
         
