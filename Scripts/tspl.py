22694e7b4821788e692bcd1371c9356643aba84f
Tim Seymore <tim_seymore2012@hotmail.com>
2020-05-26 20:28:30 -0400

    Update tspl.py

--- Scripts/tspl.py
+++ Scripts/tspl.py
@@ -2,7 +2,7 @@
 """
     TSPL - TimScriptProgrammingLanguage
 
- A functional programming language and interpreter in python
+ A simple programming language and interpreter in python
  - more of a learning device than a practical use language
 
 
@@ -11,10 +11,8 @@ Created on Sat May  9 20:24:51 2020
 @author: tim_s
 """
 
-import sys, os
+# import sys, os
 
-
-# TODO: change implementation to use pure OOP
 
 class Exp:
     """ An expression to be evaluated 
@@ -27,92 +25,130 @@ class Exp:
 
 class Int(Exp):
     """ A constant int """    
-    def __init__(self, num: int):
+    def __init__(self, i):
         super().__init__()
-        self.num = num
+        self.i = i
+        
+    def eval(self):
+        return self
+    
+    def __str__(self):
+        return str(self.i)
+    
+    def has_zero(self):
+        return self.i == 0
         
         
 class Negate(Exp):
     """ Negated expression """
-    def __init__(self, exp: Exp):
+    def __init__(self, e):
         super().__init__()
-        self.exp = exp
+        self.e = e
+        
+    def eval(self):
+        return Int(-(self.e.eval()))
+    
+    def __str__(self):
+        return "-(" + str(self.e) + ")"
+        
+    def has_zero(self):
+        return self.e.has_zero()        
   
       
 class Add(Exp):
     """ Expression representing the result
         of adding two expressions 
     """    
-    def __init__(self, exp1: Exp, exp2: Exp):
+    def __init__(self, e1, e2):
         super().__init__()
-        self.exp1 = exp1
-        self.exp2 = exp2
+        self.e1 = e1
+        self.e2 = e2
+        
+    def eval(self):
+        return Int(self.e1.eval().i + self.e2.eval().i)
+    
+    def __str__(self):
+        return "(" + str(self.e1) + " + " + str(self.e2) + ")"
+    
+    def has_zero(self):
+        return self.e1.has_zero() or self.e2.has_zero()
       
         
 class Multiply(Exp):
     """ Expression representing the result
         of multiplying two expressions 
     """
-    def __init__(self, exp1: Exp, exp2: Exp):
+    def __init__(self, e1, e2):
         super().__init__()
-        self.exp1 = exp1
-        self.exp2 = exp2
+        self.e1 = e1
+        self.e2 = e2
+        
+    def eval(self):
+        return Int(self.e1.eval().i * self.e2.eval().i)
     
+    def __str__(self):
+        return "(" + str(self.e1) + " * " + str(self.e2) + ")"
     
-class Divide(Exp):
-    """ Expression representing the result
-        of dividing two expressions 
-    """    
-    def __init(self, exp1: Exp, exp2: Exp):
-        super().__init__()
-        self.exp1 = exp1
-        self.exp2 = exp2
+    def has_zero(self):
+        self.e1.has_zero() or self.e2.has_zero()
+        
+        
+# TODO: change implementation to use pure OOP
+    
+# class Divide(Exp):
+#     """ Expression representing the result
+#         of dividing two expressions 
+#     """    
+#     def __init(self, exp1: Exp, exp2: Exp):
+#         super().__init__()
+#         self.exp1 = exp1
+#         self.exp2 = exp2
 
 
-class Variable(Exp):
-    """ A variable mapping a string to an expression """    
-    def __init__(self, name: str, value: Exp):
-        super().__init__()
-        self.name = name
-        self.value = value
+# class Variable(Exp):
+#     """ A variable mapping a string to an expression """    
+#     def __init__(self, name: str, value: Exp):
+#         super().__init__()
+#         self.name = name
+#         self.value = value
         
 
-class Pair(Exp):
-    """ A pair of expressions
+# class Pair(Exp):
+#     """ A pair of expressions
     
-       use Nul expression to end list
-       ex: Pair(e1, Pair(e2, Nul)) == [e1, e2]
-    """    
-    def __init__(self, exp1: Exp, exp2: Exp):
-        super().__init__()
-        self.exp1 = exp1
-        self.exp2 = exp2
+#        use Nul expression to end list
+#        ex: Pair(e1, Pair(e2, Nul)) == [e1, e2]
+#     """    
+#     def __init__(self, exp1: Exp, exp2: Exp):
+#         super().__init__()
+#         self.exp1 = exp1
+#         self.exp2 = exp2
         
         
-class Nul(Exp):
-    """ Used to represent a null value """    
-    def __init__(self):
-        super().__init__()
+# class Nul(Exp):
+#     """ Used to represent a null value """    
+#     def __init__(self):
+#         super().__init__()
         
 
-class Interpreter:
-    """ TSPL interpreter """    
-    def __init__(self):
-        self.run()
+# class Interpreter:
+#     """ TSPL interpreter """    
+#     def __init__(self):
+#         self.run()
         
-    def run(self):
-        while True:
-            inp: Exp = input('>>> ')
-            if inp == 'quit':
-                sys.exit()
-            self.eval_exp(inp)
+#     def run(self):
+#         while True:
+#             inp: Exp = input('>>> ')
+#             if inp == 'quit':
+#                 sys.exit()
+#             self.eval_exp(inp)
     
-    def eval_exp(self, exp: Exp):
-        print("Eval not yet implemented")
+#     def eval_exp(self, exp: Exp):
+#         print("Eval not yet implemented")
         
         
         
         
-if __name__ == "__main__":
-    Interpreter()
+# if __name__ == "__main__":
+#     Interpreter()
     
