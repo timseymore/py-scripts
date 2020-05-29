b58c8417ab514b413590c82a7ba44b75f1869c07
Tim Seymore <tim_seymore2012@hotmail.com>
2020-05-29 12:23:16 -0400

    Add connections_2.py

---
+++ Scripts/connections_2.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 29 10:48:30 2020
+
+@author: Tim
+"""
+
+n = 1000
+count = 0
+
+for i in range(n):
+    for j in range(n):
+        for k in range(n):
+            if i < j and j < k:
+                count += 1
+
+print(count)
