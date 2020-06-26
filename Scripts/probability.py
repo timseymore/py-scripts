89b9f329b7a6d6c1d8e46f05dd31d479010f3b0c
Tim Seymore <tim_seymore2012@hotmail.com>
2020-06-26 15:50:41 -0400

    Update probability.py

--- Scripts/probability.py
+++ Scripts/probability.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 """
+Probability
+
+
 Created on Fri Jun 26 11:30:03 2020
 
 @author: Tim
@@ -8,17 +11,22 @@ Created on Fri Jun 26 11:30:03 2020
 import random
 import itertools
 
+# Tossing a coin 100,000 times and checking how many 
+# heads flips there are versus tail flips,
+# we can see that about half the tosses land heads.
+# Notice that the larger the number of flips, the closer to .50 we get.
+flips = 100000
 heads = 0
 tails = 0
-
-for i in range (10000):
+for i in range (flips):
     x = random.choice(["heads", "tails"])
     if x == "heads":
         heads += 1
     else:
-        tails += 1
-        
+        tails += 1        
 print("Heads: " + str(heads))
 print("Tails: " + str(tails))
-print("Frequency of heads: " + str(abs(heads / 10000)))
+print("Frequency of heads: " + str(heads / flips))
+
+
 
